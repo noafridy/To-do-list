3f793011a358de15b130542d522dc34418ca8cbc
noa <noafridy@gmail.com>
2019-01-05 21:26:18 +0200

    todo list v2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var arr = [];
 function backup() {
     if (localStorage.getItem("arr") !== null) {
         arr = JSON.parse(localStorage.getItem("arr"));
-        for(var i=0; i<arr.length; i++){
+        for (var i = 0; i < arr.length; i++) {
             addCard(arr[i]);
         }
     }
@@ -13,11 +13,19 @@ document.addEventListener("DOMContentLoaded", function (event) {
     backup();
 });
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
 function createObj(taskData, taskDate, tasktime) {
     return {
         taskData: taskData,
         taskDate: taskDate,
-        tasktime: tasktime
+        tasktime: tasktime,
+        key: uuidv4()
     }
 }
 
@@ -54,21 +62,46 @@ function deletNewTask() {
     document.forms["addTaskNew"]["tasktime"].value = "";
 }
 
-function addCard(card) {
+function addCard(cardItem) {
     var cardContainer = document.getElementById("cardContainer");
     var div = document.createElement("div");
-    var text = document.createElement("p");
-    var date = document.createElement("p");
-    var time = document.createElement("p");
-    text.innerHTML = card.taskData;
-    date.innerHTML = card.taskDate;
-    time.innerHTML = card.tasktime;
-    div.className += "card";
-    text.className+="cardText";
-    date.className+="cardDate";
-    time.className+="cardTime";
-    div.appendChild(text);
-    div.appendChild(date);
-    div.appendChild(time);
+    div.innerHTML = "<i class='fas fa-times' onclick='removeCard(event)' data-key=" + cardItem.key + "> </i>" +
+        "<p class='cardText'>" + cardItem.taskData + "</p >" +
+        "<p class='cardDate'>" + cardItem.date + "</p>" +
+        "<p class='cardTime'> " + cardItem.time + "</p>";
+    div.className += "cardItem";
     cardContainer.append(div);
+
+    // var cardContainer = document.getElementById("cardContainer");
+    // var div = document.createElement("div");
+    // var text = document.createElement("p");
+    // var date = document.createElement("p");
+    // var time = document.createElement("p");
+    // text.innerHTML = card.taskData;
+    // date.innerHTML = card.taskDate;
+    // time.innerHTML = card.tasktime;
+    // div.className += "card";
+    // text.className+="cardText";
+    // date.className+="cardDate";
+    // time.className+="cardTime";
+    // div.appendChild(text);
+    // div.appendChild(date);
+    // div.appendChild(time);
+    // cardContainer.append(div);
+}
+
+function removeCard(event) {
+    //var cardToRemove = this.parentElement;
+    var target = event.target;
+    var key = target.dataset.key;
+    target.parentElement.remove();
+    for (var i=0;i<arr.length;i++){
+        if(key==arr[i].key){
+            arr.splice(i, 1); 
+            localStorage.setItem("arr", JSON.stringify(arr));
+            return; 
+        }
+    }
+
+    // this.parentElement.remove(this);
 }
